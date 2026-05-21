b0aa47f6c33b2cb302a434a30026da292816e8aa
Patrick J. Reed <patrickjenningsreed@gmail.com>
2026-05-20 17:46:41 -0700

    Set pytest pythonpath in pyproject.toml instead of conftest sys.path hack

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,4 @@
-import sys
-from pathlib import Path
-
 import pytest
-
-# Add tests dir to path so we can import fixtures
-sys.path.insert(0, str(Path(__file__).parent))
-
 from fixtures.make_synthetic import make_synthetic
 
 
